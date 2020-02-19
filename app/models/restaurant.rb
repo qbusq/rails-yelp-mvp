528cf38a7d6a3a8dab7e9ca2f0702091cdbad8a8
Charlotte Verhelle <chverhelle@gmail.com>
2020-02-19 15:37:23 +0100

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  CAT = %w[chinese italian japanese french belgian]
   validates :name, presence: true
   validates :address, presence: true
   validates :category,
             presence: true,
-            inclusion: { in: %w[chinese italian japanese french belgian] }
+            inclusion: { in: CAT }
 end

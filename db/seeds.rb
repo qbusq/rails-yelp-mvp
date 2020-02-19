a2ca308a070e120ee99291159c39de8b4370c34a
Charlotte Verhelle <chverhelle@gmail.com>
2020-02-19 15:07:09 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,16 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
+#
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  { name: 'De pollepel', address: 'Gentstraat 10, Brugge', category: 'belgian' },
+  { name: 'Comme Chez Soi', address: 'Rue du Boucher 13, Bruxelles', category: 'french' },
+  { name: 'Sushi Todoroki', address: 'E. Sabbelaan 5, Kortrijk', category: 'japanese' },
+  { name: 'Il Punto', address: 'Gentsesteenweg 111, Deinze', category: 'italian' },
+  { name: 'The Chinese wall', address: 'Gentsebaan 51, Waregem', category: 'chinese' }
+]
+Restaurant.create!(restaurants_attributes)
